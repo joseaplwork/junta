efd24c11d8c3916bddeb60fce6442be07ed4225e
joseaplwork <joseaplwork@gmail.com>
2025-11-08 00:35:34 +0100

    feat(admin-page): add update feature component to admin page

--- apps/admin/src/app/pages/admin/admin-page.ts
+++ apps/admin/src/app/pages/admin/admin-page.ts
@@ -4,10 +4,11 @@ import { AdminPageState } from './admin-page-state'
 import { CreateFeat } from './create-feat/create-feat'
 import { DeleteFeat } from './delete-feat/delete-feat'
 import { ListFeat } from './list-feat/list-feat'
+import { UpdateFeat } from './update-feat/update-feat'
 
 @Component({
   templateUrl: './admin-page.html',
-  imports: [ListFeat, CreateFeat, DeleteFeat],
+  imports: [ListFeat, CreateFeat, UpdateFeat, DeleteFeat],
   providers: [AdminPageState],
 })
 export class AdminPage {}
